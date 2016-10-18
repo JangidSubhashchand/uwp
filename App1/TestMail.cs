4fbd9417a82580f2a876f394f3454c05a1944c41
mkaira <m.mskaira@gmail.com>
2016-10-18 16:10:20 +0530

    sfgdfgfdg

--- App1/TestMail.cs
+++ App1/TestMail.cs
@@ -13,8 +13,43 @@ namespace App1
 
         private async void SendEmailButton_Click(object sender, RoutedEventArgs e)
         {
-            ZipFile.CreateFromDirectory(folder.Path, $"{folder.Path}\\{Guid.NewGuid()}.zip",
-                    CompressionLevel.NoCompression, true);
+            FolderPicker folderPicker = new FolderPicker();
+            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add(".dll");
+            folderPicker.FileTypeFilter.Add(".json");
+            folderPicker.FileTypeFilter.Add(".xml");
+            folderPicker.FileTypeFilter.Add(".pdb");
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                // Application now has read/write access to all contents in the picked folder (including other sub-folder contents)
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                OutputTextBlock.Text = $"Picked folder: {folder.Name}";
+
+                var files = await folder.GetFilesAsync();
+                foreach (var file in files)
+                {
+                    OutputTextBlock.Text += $"\n {file.Name}";
+                }
+
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        ZipFile.CreateFromDirectory(folder.Path, $"{folder.Path}\\{Guid.NewGuid()}.zip",
+                            CompressionLevel.NoCompression, true);
+                        Debug.WriteLine("folder zipped");
+                    }
+                    catch (Exception w)
+                    {
+                        Debug.WriteLine(w);
+                    }
+                });
+            }
+            else
+            {
+                OutputTextBlock.Text = "Operation cancelled.";
+            }
         }
     }
 }
